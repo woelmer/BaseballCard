ef54779dfa2dac11f5ac7f71975efabd8eec878f
Mike Woelmer <mike.woelmer@atomicobject.com>
2014-02-19 14:06:33 -0500

    Initial Commit

---
+++ baseballcard/BASViewController.m
@@ -0,0 +1,29 @@
+//
+//  BASViewController.m
+//  baseballcard
+//
+//  Created by Mike Woelmer on 2/19/14.
+//  Copyright (c) 2014 atomicobject. All rights reserved.
+//
+
+#import "BASViewController.h"
+
+@interface BASViewController ()
+
+@end
+
+@implementation BASViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ baseballcard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  baseballcard
+//
+//  Created by Mike Woelmer on 2/19/14.
+//  Copyright (c) 2014 atomicobject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BASAppDelegate class]));
+    }
+}
