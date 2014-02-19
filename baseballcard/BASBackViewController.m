4a34ba1babe755524ba4a01193673a924a4d5c96
Mike Woelmer <mike.woelmer@atomicobject.com>
2014-02-19 15:03:32 -0500

    Navigation

---
+++ baseballcard/BASBackViewController.m
@@ -0,0 +1,42 @@
+//
+//  BASBackViewController.m
+//  baseballcard
+//
+//  Created by Mike Woelmer on 2/19/14.
+//  Copyright (c) 2014 atomicobject. All rights reserved.
+//
+
+#import "BASBackViewController.h"
+
+@interface BASBackViewController ()
+
+@end
+
+@implementation BASBackViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)swipeAction:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
---
+++ baseballcard/BASEditFrontViewController.h
@@ -0,0 +1,13 @@
+//
+//  BASEditFrontViewController.h
+//  baseballcard
+//
+//  Created by Mike Woelmer on 2/19/14.
+//  Copyright (c) 2014 atomicobject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BASEditFrontViewController : UIViewController
+
+@end
